78bee571db59f6e48a4c07f564b5b560c2ccda01
George123321 <gosha-dem@ya.ru>
2017-11-29 03:59:52 +0300

    Почти доделал статтерм

---
+++ Информатика/C++/Лекции/lection_1/lection_13.h
@@ -0,0 +1,14 @@
+Интерфейс функции включает:
+Имя, смысл и типы параметров, допустимые значения параметров, возможные исключения (exeption).
+try {
+    do();
+    something();
+    dangerous();
+}
+catch (very specEx &x) {
+std::cout << "У нас супер-особый случай";
+}
+else if (specialEx &x) {
+if (x == msg...) {...}
+else throw;
+}
